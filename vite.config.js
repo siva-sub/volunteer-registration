05e35ff30f6086e73f8ca84459cb9d344f71d170
Sivasubramanian Ramanathan <hello@sivasub.com>
2026-01-07 15:52:50 +0800

    Move admin page to admin/index.html and update settings

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/volunteer-registration/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve('index.html'),
+                admin: resolve('admin/index.html'),
+            },
+        },
+    },
 })
